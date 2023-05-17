29feacbeb64ce62a5a4421290327d6c23e3caf28
ageorg06 <ageorg06@ucy.ac.cy>
2023-05-17 15:13:13 +0300

    First commit

--- src/web_scrap_get.py
+++ src/web_scrap_get.py
@@ -7,10 +7,13 @@ import requests
 import random
 import time
 
+import random
+from fp.fp import FreeProxy
+
+
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
-from src.utils import get_random_user_agent, get_proxy
 from selenium.webdriver.chrome.service import Service
 
 class Player:
@@ -30,6 +33,22 @@ class Player:
     def __str__(self):
         return f"{self.rank}. {self.given_name} {self.family_name} ({self.nationality}) - {self.points} points"
 
+
+def get_random_user_agent():
+    user_agents = [
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1 Safari/605.1.15'
+    ]
+    return random.choice(user_agents)
+
+
+
+
+def get_proxy():
+    proxy = FreeProxy(rand=True, timeout=1).get()
+    return proxy
 
 print(get_proxy())
 
